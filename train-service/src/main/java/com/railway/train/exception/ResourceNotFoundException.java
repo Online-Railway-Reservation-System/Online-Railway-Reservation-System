3b3d99bda745d4681735805cd0a06f56ed7e0b20
keerthana2005git <jayaganeshkeerthana@gmail.com>
2026-09-18 11:34:07 +0530

    Added config and exception files ,modified additional features

---
+++ train-service/src/main/java/com/railway/train/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.railway.train.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
